7383f2651af758efb528c8634ffb35c0a9daed52
Natan Feithlicher <feithlicher@gmail.com>
2021-06-02 14:11:47 +0300

    added init params

--- hw4/ex4-tests.py
+++ hw4/ex4-tests.py
@@ -1,10 +1,66 @@
 import numpy as np
 
-mat = np.array([[1, 2], [3, 4]])
-vec1 = np.array([1, 2])
-vec2 = np.array([3, 4])
+data = np.array([[1, 2, 10], [3, 4, 12]])
+vec1 = np.array([1, 7])
+# print(vec1)
+vec2 = np.array([4, 4])
+# nate = np.column_stack((vec1, vec2))
+# print(nate)
+# X_folds = np.split(vec1, 5, axis=0)
+# print(X_folds)
+# print(type(X_folds))
+# nate2 = np.random.uniform(1, 5)
+# print(nate2)
+#
+# nate3 = np.random.uniform(vec1, vec2)
+# print(nate3)
+# print(vec1 - vec2)
+#
+#
+# nate4 = np.zeros_like((mat[0])) + 4
+#
+# print(nate4)
 
-#print(vec1 * vec2)
-nate = np.inner(mat, vec1)
-print(np.log(2))
 
+w_init = np.zeros_like((data[0])) + (1 / 3)
+cur_mu = data.mean(axis=0)
+cur_std = data.std(axis=0)
+rand_min_mu = cur_mu - (2 * cur_std)
+rand_max_mu = cur_mu + (2 * cur_std)
+mu_init = np.random.uniform(rand_min_mu, rand_max_mu)
+std_init = [np.random.uniform(0, 3) for _ in range(data.shape[1])]
+param_mat = np.column_stack((w_init, mu_init, std_init))
+
+print(param_mat.shape)
+print(param_mat)
+
+
+
+# print(X_training.std(axis=0))
+# print(X_training.shape)
+# print(X_training[:, 0].mean())
+# print(X_training[:, 1].mean())
+
+# cur_mu = X_training.mean(axis=0)
+# cur_std = X_training.std(axis=0)
+# print("cur_mu: ", cur_mu)
+# print("cur_std: ", cur_std)
+# rand_min_mu = cur_mu - (2 * cur_std)
+# rand_max_mu = cur_mu + (2 * cur_std)
+# print(rand_min_mu)
+# print(rand_max_mu)
+# mu = np.random.uniform(rand_min_mu, rand_max_mu)
+# w = [2, 2]
+#
+# print("mu", mu)
+# std_init = [np.random.uniform(0, 3) for x in range(2)]
+# print("std_init", std_init)
+# print(np.column_stack((w, mu, std_init)))
+
+
+# cur_mu = cur_mu / 2
+# print(cur_mu)
+# dim_ind_vec = [x + 1 for x in range(2)]
+# cur_mu = cur_mu * dim_ind_vec
+# print(dim_ind_vec)
+# print(cur_mu)
--- hw4/ex4.py
+++ hw4/ex4.py
@@ -7,6 +7,7 @@ training_set = pd.read_csv('training_set.csv')
 test_set = pd.read_csv('test_set.csv')
 X_training, y_training = training_set[['x1', 'x2']].values, training_set['y'].values
 X_test, y_test = test_set[['x1', 'x2']].values, test_set['y'].values
+np.random.seed(42)
 
 
 def compute_cost(X, y, theta):
@@ -41,7 +42,7 @@ class LogisticRegressionGD(object):
         self.random_state = random_state
         np.random.seed(self.random_state)
         self.theta = np.random.random(size=theta_shape)
-        self.cost_list = []
+        self.cost_list = [] # a list of the costs that you've calculated in each iteration
 
     def fit(self, X, y):
         """
@@ -81,13 +82,82 @@ class LogisticRegressionGD(object):
 
     def predict(self, X):
         """Return the predicted class label"""
+        sigma = 1 / (1 + (np.e ** ((-1) * (np.inner(self.theta, X)))))
+        if sigma > 0.5:
+            return 1
+        else:
+            return 0
+
+
+# calc normal pdf
+def norm_pdf(data, mu, sigma):
+    exp = (((data - mu) / sigma) ** 2) / (-2)
+    base_inv = sigma * ((2 * np.pi) ** 0.5)
+    return (np.e ** exp) / base_inv
+
+
+class EM(object):
+    """
+    Naive Bayes Classifier using Gauusian Mixture Model (EM) for calculating the likelihood.
+
+    Parameters
+    ------------
+    k : int
+      Number of gaussians in each dimension
+    n_iter : int
+      Passes over the training dataset in the EM proccess
+    eps: float
+      minimal change in the cost to declare convergence
+    random_state : int
+      Random number generator seed for random params initialization.
+    """
+
+    def __init__(self, k=1, n_iter=1000, eps=0.01):
+        self.k = k
+        self.n_iter = n_iter
+        self.eps = eps
+        self.param_mat = []
+
+    # initial guesses for parameters
+    def init_params(self, data):
+        """
+        Initialize distribution params:
+        Every row corresponds to a column in data : [w, mu, sigma]
+        """
+        w_init = np.zeros_like((data[0])) + (1 / self.k)
+        cur_mu = data.mean(axis=0)
+        cur_std = data.std(axis=0)
+        rand_min_mu = cur_mu - (2 * cur_std)
+        rand_max_mu = cur_mu + (2 * cur_std)
+        mu_init = np.random.uniform(rand_min_mu, rand_max_mu)
+        std_init = [np.random.uniform(0, 3) for _ in range(data.shape[1])]
+        self.param_mat = np.column_stack((w_init, mu_init, std_init))
+
+    def expectation(self, data):
+        """
+        E step - calculating responsibilities
+        """
         pass
 
+    def maximization(self, data):
+        """
+        M step - updating distribution params
+        """
+        pass
 
+    def fit(self, data):
+        """
+        Fit training data (the learning phase).
+        Use init_params and then expectation and maximization function in order to find params
+        for the distribution.
+        Store the params in attributes of the EM object.
+        Stop the function when the difference between the previous cost and the current is less than eps
+        or when you reach n_iter.
+        """
+        pass
 
-
-
-
+    def get_dist_params(self):
+        pass
 
 
 
