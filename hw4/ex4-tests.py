81f52228068d079b911d5b7b6aae5d8b2ba7ac93
Natan Feithlicher <feithlicher@gmail.com>
2021-06-06 23:49:52 +0300

    done :)

--- hw4/ex4-tests.py
+++ hw4/ex4-tests.py
@@ -6,15 +6,23 @@ test_set = pd.read_csv('test_set.csv')
 X_training, y_training = training_set[['x1', 'x2']].values, training_set['y'].values
 X_test, y_test = test_set[['x1', 'x2']].values, test_set['y'].values
 
-# mat - rows are linearly dependent
-mat = np.array([[1, 2], [2, 4]])
+# row_dependent_mat
+row_dependent_mat = np.array([[1, 2], [2, 4]])
 for i in range(1998):
-    mat = np.row_stack((mat, (mat[(i + 1)] * (1.001))))
+    row_dependent_mat = np.row_stack((row_dependent_mat, (row_dependent_mat[(i + 1)] * (1.001))))
 zero_lebals = np.array([0 for _ in range(500)])
 one_lebals = np.array([1 for _ in range(500)])
 labels = np.concatenate((zero_lebals, one_lebals, zero_lebals, one_lebals))
-mat = np.column_stack((mat, labels))
-mat = pd.DataFrame(mat)
-mat.columns = ["x1", "x2", "y"]
+row_dependent_mat = np.column_stack((row_dependent_mat, labels))
+row_dependent_mat = pd.DataFrame(row_dependent_mat)
+row_dependent_mat.columns = ["x1", "x2", "y"]
 
-print(mat)
+# column_dependent_mat
+left_column = np.array([i for i in range(1, 2001)])
+right_column = 2 * left_column
+column_dependent_mat = np.column_stack((left_column, right_column))
+column_dependent_mat = np.column_stack((column_dependent_mat, labels))
+column_dependent_mat = pd.DataFrame(column_dependent_mat)
+column_dependent_mat.columns = ["x1", "x2", "y"]
+
+print(column_dependent_mat)
